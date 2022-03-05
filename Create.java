7c5fa6b8bb5c0500574175c373e13ee164cd6aaa
CrusaderCoder73 <mbustam9@msudenver.edu>
2022-03-04 18:49:46 -0700

    Created class templates for create update and delete

---
+++ Create.java
@@ -0,0 +1,15 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+public class Create {
+	/*
+	 * Create Class Constructor
+	 */
+	public Create() {
+		
+	}
+	
+	public void create(String productID, String quantity, float wholeSale, float sale, String supplierID) {
+		
+	}
+}
---
+++ Delete.java
@@ -0,0 +1,15 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+public class Delete {
+	/*
+	 * Delete class constructor
+	 */
+	public Delete() {
+		
+	}
+	
+	public void delete(String productID) {
+		
+	}
+}
