845467bab117724393f0167bcc189afc0f65ecd8
natek <natek@10.0.0.171>
2022-04-03 20:34:46 -0600

    added JTable to the GUI and added methods to read class

--- src/main/java/main/GUI.java
+++ src/main/java/main/GUI.java
@@ -1,6 +1,9 @@
 package main;
+import crudOperations.*;
 
 import java.awt.EventQueue;
+
+import javax.swing.JFrame;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -12,13 +15,18 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Insets;
 import java.awt.Color;
-import crudOperations.*;
+import main.DataTransfer;
+import javax.swing.JTable;
+import java.awt.Component;
+import javax.swing.JScrollPane;
+
 
 
 public class GUI {
 
 	private JFrame frame;
-
+	private JTable table;
+	private JTable table_1;
 	/**
 	 * Launch the application.
 	 */
@@ -34,14 +42,13 @@ public class GUI {
 			}
 		});
 	}
-
 	/**
 	 * Create the application.
 	 */
 	public GUI() {
 		initialize();
+		
 	}
-
 	/**
 	 * Initialize the contents of the frame.
 	 */
@@ -49,20 +56,22 @@ public class GUI {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	/**
-	 * Start of Grid layout code
-	 */
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, Double.MIN_VALUE};
-		frame.getContentPane().setLayout(gridBagLayout);
 	
 	/**
 	 * button for read; no functionality 
 	 */
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{88, 0, 0, 55, 63, 65, 61, 0};
+		gridBagLayout.rowHeights = new int[]{21, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		frame.getContentPane().setLayout(gridBagLayout);
+	
+	/**
+	 * button for create; no functionality
+	 */
 		JButton btnNewButton = new JButton("Read");
+		btnNewButton.setAlignmentY(Component.TOP_ALIGNMENT);
 		btnNewButton.addActionListener(new ActionListener() {
 
 			//Read Button
@@ -71,7 +80,7 @@ public class GUI {
 				String input = JOptionPane.showInputDialog(null, "Input Product ID");
 				// calling read method and storing it into a variable
 				DataTransfer readResults = reader.read(input);
-				
+
 				//Storing data into variables
 				String id = readResults.getInventoryID();
 				int quantity = readResults.getInventoryQuantity();
@@ -85,46 +94,68 @@ public class GUI {
 			}
 		});
 		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-		gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
-		gbc_btnNewButton.gridx = 0;
+		gbc_btnNewButton.anchor = GridBagConstraints.NORTHWEST;
+		gbc_btnNewButton.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNewButton.gridx = 3;
 		gbc_btnNewButton.gridy = 0;
 		frame.getContentPane().add(btnNewButton, gbc_btnNewButton);
-	
-	/**
-	 * button for create; no functionality
-	 */
 		JButton btnNewButton_1 = new JButton("Create");
 		btnNewButton_1.setBackground(Color.WHITE);
 		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
-		gbc_btnNewButton_1.insets = new Insets(0, 0, 0, 5);
-		gbc_btnNewButton_1.gridx = 1;
+		gbc_btnNewButton_1.anchor = GridBagConstraints.NORTHWEST;
+		gbc_btnNewButton_1.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNewButton_1.gridx = 4;
 		gbc_btnNewButton_1.gridy = 0;
 		frame.getContentPane().add(btnNewButton_1, gbc_btnNewButton_1);
-		
-	/**
-	 * button for Update; no functionality 
-	 */
 		JButton btnNewButton_2 = new JButton("Update");
 		btnNewButton_2.setBackground(Color.WHITE);
 		GridBagConstraints gbc_btnNewButton_2 = new GridBagConstraints();
-		gbc_btnNewButton_2.insets = new Insets(0, 0, 0, 5);
-		gbc_btnNewButton_2.gridx = 2;
+		gbc_btnNewButton_2.anchor = GridBagConstraints.NORTHWEST;
+		gbc_btnNewButton_2.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNewButton_2.gridx = 5;
 		gbc_btnNewButton_2.gridy = 0;
 		frame.getContentPane().add(btnNewButton_2, gbc_btnNewButton_2);
-	
-	/**
-	 * Button for Delete; no functionality
-	 */
 		
 		JButton btnNewButton_3 = new JButton("Delete");
 		btnNewButton_3.setForeground(Color.RED);
 		btnNewButton_3.setBackground(Color.WHITE);
 		GridBagConstraints gbc_btnNewButton_3 = new GridBagConstraints();
-		gbc_btnNewButton_3.insets = new Insets(0, 0, 0, 5);
-		gbc_btnNewButton_3.gridx = 3;
+		gbc_btnNewButton_3.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNewButton_3.anchor = GridBagConstraints.NORTHWEST;
+		gbc_btnNewButton_3.gridx = 6;
 		gbc_btnNewButton_3.gridy = 0;
 		frame.getContentPane().add(btnNewButton_3, gbc_btnNewButton_3);
+		
+		//creates read object to fill entries of data
+		Read reader = new Read();
+		Object[][] data = reader.readAll();
+		
+		String column[]={"Product ID","Quantity","Wholesale Price", "Sale Price", "Supplier ID"};
+		
+		JTable table_1 = new JTable( data, column);
+		
+		Object[] row = new Object [5];
+		
+		GridBagConstraints gbc_table_1 = new GridBagConstraints();
+		gbc_table_1.anchor = GridBagConstraints.NORTH;
+		gbc_table_1.insets = new Insets(0, 0, 0, 5);
+		gbc_table_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_table_1.gridx = 0;
+		gbc_table_1.gridy = 4;
+		frame.getContentPane().add(table_1, gbc_table_1);
+		
+		//This allows the table to scroll
+		JScrollPane scrollPane_1 = new JScrollPane(table_1);
+		GridBagConstraints gbc_scrollPane_1 = new GridBagConstraints();
+		gbc_scrollPane_1.insets = new Insets(0, 0, 0, 5);
+		gbc_scrollPane_1.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane_1.gridx = 0;
+		gbc_scrollPane_1.gridy = 4;
+		frame.getContentPane().add(scrollPane_1, gbc_scrollPane_1);
+		
 	}
+	
+	
 
 }
 
